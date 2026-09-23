5bdaf37d05586bf7780344d8aadcc4e83b0b990f
ridho <ridho@gmail.com>
2026-09-24 00:44:06 +0800

    fix: add favicon link to the layout for improved branding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }} &mdash; BBGTK Sulsel</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('landing/images/favicon.ico') }}">
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
